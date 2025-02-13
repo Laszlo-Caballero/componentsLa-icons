d243675964f6dfd1e469c9d2d8b3fbaf5052e752
laszlo-caballero <laszlocaballero@gmail.com>
2025-02-12 23:55:55 -0500

    feat(parser): mejorar el manejo de atributos SVG para incluir nuevos casos y condiciones

--- Svgs/Parser.py
+++ Svgs/Parser.py
@@ -3,30 +3,26 @@ def Parser(codigo: str):
     palabras_eliminar = ["width", "height"]
     palabras_reemplazar = ["fill", "stroke"]
     
-    
-    palabras_no_cammel_case = ["xmlns:xlink", "xml:space"]
-    palabras_cammel_case = ["xmlnsXlink", "xmlSpace"]
+    palabras_no_cammel_case = ["xmlns:xlink", "xml:space", "stroke-linecap", "stroke-linejoin", "fill-rule", "clip-rule"]
+    palabras_cammel_case = ["xmlnsXlink", "xmlSpace", "strokeLinecap", "strokeLinejoin", "fillRule", "clipRule"]
     
     # Eliminamos las palabras que no necesitamos
     for palabra in palabras_eliminar:
         codigo = re.sub(f'{palabra}=".*?"', "", codigo)
     
-    #Reemplazamos las palabras que necesitamos
+    # Reemplazamos las palabras que necesitamos
     for palabra in palabras_reemplazar:
-        codigo = re.sub(f'{palabra}=".*?"', f'{palabra}="currentColor"', codigo)
+        if palabra == "fill":
+            codigo = re.sub(f'{palabra}="(?!none).*?"', f'{palabra}="currentColor"', codigo)
+        else:
+            codigo = re.sub(f'{palabra}=".*?"', f'{palabra}="currentColor"', codigo)
     
-    
-    #Reemplazamos las palabras que necesitamos
+    # Reemplazamos las palabras que necesitamos
     for i in range(len(palabras_no_cammel_case)):
         codigo = re.sub(f'{palabras_no_cammel_case[i]}', f'{palabras_cammel_case[i]}', codigo)
     
-    
-    
     # Añadir al final del primer > el atributo {...props} solo el primero
     codigo = re.sub(">", " {...props}>", codigo, 1)
-    
-    
-    
     
     return codigo
 
